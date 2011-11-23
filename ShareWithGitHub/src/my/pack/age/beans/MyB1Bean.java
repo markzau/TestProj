e85ae7aa6413b83ed30c3d8a16ef44d265fa2d8c
Mark Ziebell <mziebell@cirrustech.com.au>
2011-11-23 12:55:48 +1100

    commit on B1 (initially)

---
+++ ShareWithGitHub/src/my/pack/age/beans/MyB1Bean.java
@@ -0,0 +1,16 @@
+package my.pack.age.beans;
+
+public class MyB1Bean {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
